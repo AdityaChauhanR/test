cb1c6dba6c5f6a85ed555acac772fde8df15a4fd
Aditya5987 <adityachauhan053@gmail.com>
2025-04-24 18:27:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-module.exports = {
+module.exports2 = {
     port:process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'song_track',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || '',
+        options : {
+            dialect: process.env.Dialect || 'mysql',
+            host: process.env.HOST || 'localhost',
+            storage: './tabtracker.mysql'
+        }
+    }
+}
+
+module.exports1 = {
+    port:process.env.PORT || 8082,
     db: {
         database: process.env.DB_NAME || 'song_track',
         user: process.env.DB_USER || 'root',
